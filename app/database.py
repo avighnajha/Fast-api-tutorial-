074eaebd9bd5917b88f60d06092e4803fb308085
Avighna Jha <avighnajha1703@gmail.com>
2022-08-03 23:18:34 +0530

    Connection to databse working and moved from raw sql to sql alchemy(orm)

---
+++ app/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from urllib.parse import quote_plus as urlquote
+
+ 
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:%s@localhost/fastapi" % urlquote("Aviati@123")
+
+engine = create_engine("postgresql://postgres:%s@localhost/fastapi" % urlquote("Aviati@123"))
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base() 
+
