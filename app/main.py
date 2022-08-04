9af8a453bb5f6108717904d1cce460b00b51596e
Avighna Jha <avighnajha1703@gmail.com>
2022-08-04 11:21:08 +0530

    Cleaned up code using routers

--- app/main.py
+++ app/main.py
@@ -15,13 +15,16 @@ from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor 
 import time
-from . import models, schemas
+from . import models, schemas, utils
 from sqlalchemy.orm import Session
 from .database import engine, SessionLocal
+from .routers import post, user
+
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
 
 def get_db():
     db = SessionLocal()
@@ -29,6 +32,7 @@ def get_db():
         yield db
     finally:
         db.close()
+
 
 while True:
     try:
@@ -55,82 +59,15 @@ def find_index_post(id):
         if p["id"] == id:
             return i
 
+
+app.include_router(post.router)
+app.include_router(user.router)
+
+
 @app.get("/")
 def root():
     return { "message": "Hello World"}
 
 
-@app.get("/posts", response_model = List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts""")
-    # posts = cursor.fetchall()
-    posts = db.query(models.Post).all()
-    return posts
 
-
-@app.post("/posts", status_code = status.HTTP_201_CREATED, response_model= schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
-    # cursor.execute("""INSERT INTO posts ("title", "content", "published") VALUES (%s, %s, %s) RETURNING *""", (post.title, post.content, post.published))
-    # new_post = cursor.fetchone()
-    # conn.commit()
-    new_post = models.Post(**post.dict())
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-
-    return new_post
-
-@app.get("/posts/{id}", response_model = schemas.Post)
-def get_post(id: int, response: Response, db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts WHERE "id" = %s""", (str(id)),)
-    # post = cursor.fetchone()
-
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} was not found")
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {"message": f"post with id: {id} was not found"}
-    return post
-
-@app.delete("/posts/{id}", status_code = status.HTTP_204_NO_CONTENT)
-def delete_post(id:int, db: Session = Depends(get_db)):
-    # cursor.execute("""DELETE FROM posts WHERE "id" = %s returning *""", (str(id)))
-    # deleted_post = cursor.fetchone()
-
-    post = db.query(models.Post).filter(models.Post.id == id)
-
-    if post.first() == None:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail= f"post with id: {id} does not exist")
-    post.delete(synchronize_session=False)
-    db.commit()
-
-    return Response(status_code = status.HTTP_204_NO_CONTENT)
-
-@app.put("/posts/{id}")
-def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
-    # cursor.execute("""UPDATE posts SET "title" = %s, "content"=%s, "published"= %s WHERE "id" = %s RETURNING *""", (post.title, post.content, post.published, id))
-    # updated_post = cursor.fetchone()
-    # conn.commit()
-
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-    post = post_query.first()
-
-    if post == None:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail= f"post with id: {id} does not exist")
-    post_query.update({"title": updated_post.title,"content": updated_post.content},synchronize_session=False)
-    db.commit()
-    return post_query.first()
-
-
-#USER AUTHENTICATION
-
-@app.post("/users", status_code = status.HTTP_201_CREATED, response_model = schemas.UserOut)
-def create_user(user: schemas.UserCreate,db: Session = Depends(get_db)):
-    new_user = models.User(**user.dict())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-
-    return new_user
 
